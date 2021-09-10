1d42d7ce80af5b476f7e07c40214968a22acd0a4
ymx <ymx@autobot.com>
2021-09-10 18:37:23 +0800

    init

---
+++ index.js
@@ -0,0 +1,61 @@
+import request from 'request'
+
+export default class Email{
+    constructor(token=""){
+        this.token = token
+    }
+    verify(address){
+        const options = {
+            method: 'POST',
+            url: 'https://sendpalm.com/api/check_email',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': this.token
+            },
+            body: {
+                toEmail: address
+            },
+            json: true
+        }
+        console.log("options: ",options)
+        return new Promise(function(resolve,reject){
+            request(options, (error, response, body) => {
+                // if (error) throw new Error(error)
+                // console.log(body)
+                if(error){
+                    reject(new Error(error))
+                }
+                resolve(body)
+            })
+        })
+        
+    }
+    send(fromEmail, toEmail, subject, content){
+        const options = {
+            method: 'POST',
+            url: 'https://sendpalm.com/api/send_email',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': this.token
+            },
+            body: {
+                "fromEmail":fromEmail,
+                "toEmail":toEmail,
+                "subject":subject,
+                "content":content
+            },
+            json: true
+        }
+        console.log("options: ",options)
+        return new Promise(function(resolve,reject){
+            request(options, (error, response, body) => {
+                // if (error) throw new Error(error)
+                // console.log(body)
+                if(error){
+                    reject(new Error(error))
+                }
+                resolve(body)
+            })
+        })
+    }
+} 
